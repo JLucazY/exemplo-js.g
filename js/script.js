4be4935eba12588103b3733a1c0a5e482b3b9bb8
JLucasY <rm98458@fiap.com.br>
2023-03-07 11:49:01 -0300

    script.js adicionado ao html

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//este é um comentário de linha 
+
+/**
+ * é um 
+ * com
+ * asfjçlks
+ * dsklfkljsfsd
+ * lkjadlçkfjsa
+ */
+//Exemplo de funções do console 
+console.log("Olá mundo")
+console.error("Bom dia")
+console.warn("Será?")
+console.info("Vai ser...")
